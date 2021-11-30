93ef66e8e3babd6435fb656cd8e36d74b90e8d42
Cekic, Vukan <Vukan.Cekic@wolterskluwer.com>
2021-11-30 21:57:21 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const scrape = require("./services/scrape");
+const config = require("./config/config.json");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+
+app.set("view engine", "ejs");
+
+app.get("", async (req, res) => {
+  try {
+    const arr = await scrape.getPriceFeed();
+    res.status(200).render("price-feed", { top10: arr });
+  } catch (err) {
+    res.status(500).render("error");
+  }
+});
+
+app.listen(config.PORT, () => {
+  console.log(`started on port ${config.PORT}`);
+});
---
+++ services/scrape.js
@@ -0,0 +1,53 @@
+const config = require("../config/config.json");
+const chererio = require("cheerio");
+const axios = require("axios");
+
+module.exports.getPriceFeed = async function getPriceFeed() {
+  const siteUrl = config.SCRAPE_URL;
+  const { data } = await axios({
+    method: "GET",
+    url: siteUrl,
+  });
+  // throw new Error("Ooops"); To check error handeling
+  return scrapeData(data, 9);
+};
+
+function scrapeData(data, numberToScrape) {
+  const keys = [
+    "rank",
+    "name",
+    "price",
+    "24h",
+    "7d",
+    "marketCap",
+    "volume",
+    "cirulatingSupply",
+  ];
+  const coinArr = [];
+
+  const $ = chererio.load(data);
+
+  $(config.SCRAPE_SELECTOR).each((parentIndex, parentElm) => {
+    let keyIndex = 0;
+    const coinObj = {};
+
+    if (parentIndex <= numberToScrape) {
+      $(parentElm)
+        .children()
+        .each((childIndex, childElm) => {
+          let value = $(childElm).text();
+
+          if (keyIndex === 1 || keyIndex === 6) {
+            value = $("p:first-child", $(childElm).html()).text();
+          }
+
+          if (value) {
+            coinObj[keys[keyIndex]] = value;
+            keyIndex++;
+          }
+        });
+      coinArr.push(coinObj);
+    }
+  });
+  return coinArr;
+}
